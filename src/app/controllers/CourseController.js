871824837daa0e64c5e9530a8721e4db23c2a9ef
THIENNHAN <93808313+lamnhann@users.noreply.github.com>
2023-12-25 14:06:17 +0700

    [CRUD] Create new course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -11,6 +11,23 @@ class CourseController {
             .catch(next);
         
     }
+
+    // [GET] /courses/create
+    create(req, res, next) {
+        res.render('courses/create')  
+    }
+
+    // [POST] /courses/store
+    store(req, res, next) {
+        const formData = req.body
+        formData.image = `https://i.ytimg.com/vi/${req.body.videoId}/hqdefault.jpg`
+        const course = new Course(formData)
+        course.save()
+            .then(() => res.redirect(`/`))
+            .catch(error => {
+                
+            })
+    }
 }
 
 module.exports = new CourseController();
