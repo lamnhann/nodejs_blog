67c592c401d552fab0a24da0c36b294b4a1553d5
THIENNHAN <93808313+lamnhann@users.noreply.github.com>
2023-12-30 20:31:39 +0700

    Deleted count documents

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,11 +4,31 @@ class MeController {
     
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then(courses => res.render('me/stored-courses', {
-                courses: mutipleMongooseToObject(courses)
-            }))
+
+        Promise.all([Course.find({}),Course.countDocumentsDeleted()])
+            // Promise.all sẽ trả về một promise dạng mảng
+            // courses là kết quả của promise .then(courses), deletedCount là kết quả của promise .then(deletedCount) 
+            // [courses, deletedCount] là sử dụng destructuring bóc tách các phần tử trong mảng
+            .then(([courses, deletedCount]) => 
+                res.render('me/stored-courses', {
+                    deletedCount,
+                    courses: mutipleMongooseToObject(courses)
+                })
+            )
             .catch(next)
+        
+        // Gộp 2 thằng promise này lại chung
+        // Course.countDocumentsDeleted()
+        //     .then(deletedCount => {
+        //         console.log(deletedCount)
+        //     })
+        //     .catch(next);
+
+        // Course.find({})
+        //     .then(courses => res.render('me/stored-courses', {
+        //         courses: mutipleMongooseToObject(courses)
+        //     }))
+        //     .catch(next)
         
     }
 
